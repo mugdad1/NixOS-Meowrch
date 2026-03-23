a28d8c7711d57e63f634c59fdcd68404f39b821c
mugdad <mugdad02@tutamail.com>
2026-03-23 10:34:47 +0300

    d

--- flake.nix
+++ flake.nix
@@ -22,113 +22,162 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        # IMPORTANT: To ensure compatibility with the latest Firefox version, use nixpkgs-unstable.
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    home-manager,
-    hyprland,
-    hyprland-plugins,
-    spicetify-nix,
-    catppuccin-nix,
-    firefox-addons,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      hyprland,
+      hyprland-plugins,
+      spicetify-nix,
+      catppuccin-nix,
+      firefox-addons,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-
-    pkgs-unstable = import nixpkgs-unstable {
-      inherit system;
-      config.allowUnfree = true;
-    };
-
-    # Overlay с кастомными пакетами
-    overlay-meowrch = final: prev: (import ./pkgs {pkgs = final;});
-    overlay-portal-gbm-fix = import ./overlays/portal-gbm-fix.nix;
-
-    userConfigPath =
-      if builtins.pathExists ./hosts/meowrch/user-local.nix
-      then ./hosts/meowrch/user-local.nix
-      else ./hosts/meowrch/user.nix;
-    userConfig = import userConfigPath;
-    meowrchUser = userConfig.meowrch.user or "meowrch";
-    meowrchHostname = userConfig.meowrch.hostname or "meowrch-machine";
-  in {
-    nixosConfigurations.meowrch = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit inputs spicetify-nix catppuccin-nix hyprland hyprland-plugins pkgs-unstable;
-        inherit meowrchUser meowrchHostname;
-        firefox-addons = inputs.firefox-addons.packages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
       };
-      modules = [
-        ({pkgs, ...}: {
-          nixpkgs.overlays = [
-            overlay-meowrch
-            overlay-portal-gbm-fix
-          ];
-        })
 
-        ./hosts/meowrch/configuration.nix
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
 
-        home-manager.nixosModules.home-manager
+      # Overlay с кастомными пакетами
+      overlay-meowrch = final: prev: (import ./pkgs { pkgs = final; });
+      overlay-portal-gbm-fix = import ./overlays/portal-gbm-fix.nix;
+
+      userConfigPath =
+        if builtins.pathExists ./hosts/meowrch/user-local.nix then
+          ./hosts/meowrch/user-local.nix
+        else
+          ./hosts/meowrch/user.nix;
+      userConfig = import userConfigPath;
+      meowrchUser = userConfig.meowrch.user or "meowrch";
+      meowrchHostname = userConfig.meowrch.hostname or "meowrch-machine";
+    in
+    {
+      nixosConfigurations.meowrch = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit
+            inputs
+            spicetify-nix
+            catppuccin-nix
+            hyprland
+            hyprland-plugins
+            pkgs-unstable
+            ;
+          inherit meowrchUser meowrchHostname;
+          firefox-addons = inputs.firefox-addons.packages.${system};
+        };
+        modules = [
+          (
+            { pkgs, ... }:
+            {
+              nixpkgs.overlays = [
+                overlay-meowrch
+                overlay-portal-gbm-fix
+              ];
+            }
+          )
+
+          ./hosts/meowrch/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.extraSpecialArgs = {
+              inherit
+                inputs
+                pkgs-unstable
+                meowrchUser
+                meowrchHostname
+                ;
+              firefox-addons = inputs.firefox-addons.packages.${system};
+            };
+
+            home-manager.users.${meowrchUser} = {
+              imports = [
+                inputs.spicetify-nix.homeManagerModules.default
+                inputs.catppuccin-nix.homeModules.catppuccin
+                ./hosts/meowrch/home.nix
+              ];
+            };
+
+            home-manager.backupFileExtension = "backup";
+          }
+
+          ./modules/nixos/desktop/hyprland.nix
+        ];
+      };
+
+      # Standalone home-manager (опционально)
+      homeConfigurations.meowrch = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit
+            inputs
+            pkgs-unstable
+            meowrchUser
+            meowrchHostname
+            ;
+          firefox-addons = inputs.firefox-addons.packages.${system};
+        };
+        modules = [
+          inputs.spicetify-nix.homeManagerModules.default
+          inputs.catppuccin-nix.homeModules.catppuccin
+          ./hosts/meowrch/home.nix
+        ];
+      };
+
+      formatter.${system} = pkgs.alejandra;
+
+      packages.${system} =
+        let
+          customPkgs = import ./pkgs { inherit pkgs; };
+        in
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+          inherit (customPkgs)
+            fabric
+            fabric-cli
+            mewline
+            pawlette
+            meowrch-themes
+            hotkeyhub
+            meowrch-settings
+            meowrch-scripts
+            meowrch-tools
+            ;
+        };
 
-          home-manager.extraSpecialArgs = {
-            inherit inputs pkgs-unstable meowrchUser meowrchHostname;
-            firefox-addons = inputs.firefox-addons.packages.${system};
-          };
-
-          home-manager.users.${meowrchUser} = {
-            imports = [
-              inputs.spicetify-nix.homeManagerModules.default
-              inputs.catppuccin-nix.homeModules.catppuccin
-              ./hosts/meowrch/home.nix
-            ];
-          };
-
-          home-manager.backupFileExtension = "backup";
-        }
-
-        ./modules/nixos/desktop/hyprland.nix
-      ];
-    };
-
-    # Standalone home-manager (опционально)
-    homeConfigurations.meowrch = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = {
-        inherit inputs pkgs-unstable meowrchUser meowrchHostname;
-        firefox-addons = inputs.firefox-addons.packages.${system};
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          git
+          nixd
+          nil
+          alejandra
+        ];
       };
-      modules = [
-        inputs.spicetify-nix.homeManagerModules.default
-        inputs.catppuccin-nix.homeModules.catppuccin
-        ./hosts/meowrch/home.nix
-      ];
     };
-
-    formatter.${system} = pkgs.alejandra;
-
-    packages.${system} = let
-      customPkgs = import ./pkgs {inherit pkgs;};
-    in {
-      inherit (customPkgs) fabric fabric-cli mewline pawlette meowrch-themes hotkeyhub meowrch-settings meowrch-scripts meowrch-tools;
-    };
-
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [ git nixd nil alejandra ];
-    };
-  };
 }
---
+++ modules/home/zen-browser.nix
@@ -0,0 +1,15 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    inputs.zen-browser.homeModules.beta
+  ];
+
+  programs.zen-browser = {
+    enable = true;
+    setAsDefaultBrowser = true;
+  };
+}
