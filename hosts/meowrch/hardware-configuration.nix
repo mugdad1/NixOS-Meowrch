b414ef334e7d2fb922738204b12753403d9ee4dd
mugdad <mugdad02@tutamail.com>
2026-03-23 11:47:28 +0300

    d

--- hosts/meowrch/hardware-configuration.nix
+++ hosts/meowrch/hardware-configuration.nix
@@ -19,6 +19,8 @@
     "thunderbolt"
     "nvme"
   ];
+
+  # Keep i915 in initramfs (kernel driver for Iris Xe)
   boot.initrd.kernelModules = [ "i915" ];
   boot.kernelModules = [
     "kvm-intel"
@@ -27,19 +29,20 @@
   ];
   boot.extraModulePackages = [ ];
 
-  ############################################
-  # Intel kernel tuning
-  ############################################
+  # i915 GuC + runtime/power params
   boot.kernelParams = [
-    "i915.enable_psr=1"
+    "i915.enable_guc=3"
     "i915.fastboot=1"
+    "i915.enable_rc6=1"
+    "i915.enable_dc=1"
+    "i915.enable_psr=1"
   ];
 
   ############################################
-  # Increase virtual memory map count (large games / proton)
+  # vm maps
   ############################################
   boot.kernel.sysctl = {
-    "vm.max_map_count" = 2147483642;
+    "vm.max_map_count" = 262144;
   };
 
   ############################################
@@ -67,42 +70,44 @@
   services.xserver.videoDrivers = lib.mkIf config.services.xserver.enable [ "modesetting" ];
 
   ############################################
-  # Modern graphics stack with 32-bit support
+  # Graphics stack with 32-bit support
   ############################################
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
 
-    extraPackages = with pkgs; [
-      mesa
-      libva
-      libva-utils
-      libdrm
-      vulkan-loader
-      vulkan-validation-layers
-      vulkan-extension-layer
-      intel-media-driver # iHD driver (Broadwell+, recommended)
-      intel-vaapi-driver # i965 driver (older GPUs fallback)
-      libva-vdpau-driver # VDPAU backend for VA-API
-      libvdpau-va-gl # VDPAU via VA-API
-      vpl-gpu-rt # Intel oneVPL GPU runtime
-    ];
-
-    extraPackages32 = with pkgs.driversi686Linux; [
-      mesa
-    ];
+    extraPackages =
+      with pkgs;
+      [
+        mesa
+        libva
+        libva-utils
+        libdrm
+        vulkan-loader
+        vulkan-tools
+        vulkan-validation-layers
+        vulkan-extension-layer
+        (lib.optional (builtins.hasAttr "vulkan-intel" pkgs) pkgs.vulkan-intel)
+        intel-media-driver
+        intel-vaapi-driver
+        libva-vdpau-driver
+        libvdpau-va-gl
+        vpl-gpu-rt
+      ]
+      ++ lib.optional (builtins.hasAttr "driversi686Linux" pkgs) (with pkgs.driversi686Linux; [ mesa ]);
   };
 
   ############################################
-  # Environment variables for Intel VA-API
+  # Environment variables to prefer iris userspace driver
   ############################################
   environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "iHD"; # Use intel-media-driver (Broadwell+)
-    # For older GPUs, change to "i965"
+    LIBVA_DRIVER_NAME = "iHD";
+    VAAPI_DRIVERS_PATH = "/run/opengl-driver/lib/dri";
+    MESA_LOADER_DRIVER_OVERRIDE = "iris";
   };
 
   ############################################
-  # Scanner support (hardware capability)
+  # Scanner support
   ############################################
   hardware.sane = {
     enable = true;
@@ -116,9 +121,14 @@
   # Common userland tooling / diagnostics
   ############################################
   environment.systemPackages = with pkgs; [
-    # Capability / info (glxinfo is part of mesa-demos)
+    linux-firmware
+    intel-microcode
     vulkan-tools
     glmark2
+
+    # Mesa demos (glxinfo) and VA-API utils (vainfo)
+    mesa-demos
+    libva-utils
 
     # Wayland utilities
     wlr-randr
@@ -146,7 +156,7 @@
   ];
 
   ############################################
-  # Common runtime filesystem expectations
+  # Runtime tmpfiles
   ############################################
   systemd.tmpfiles.rules = [
     "d /tmp/.X11-unix 1777 root root 10d"
@@ -157,5 +167,5 @@
   # Hardware configuration
   ############################################
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
 }
